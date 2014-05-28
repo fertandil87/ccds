96d1055f8e37964bbcec93f78343ea913a6b394f
Fernando Saez <fertandil87@gmail.com>
2014-05-28 20:23:06 -0300

    added comments

--- js/app.js
+++ js/app.js
@@ -8,13 +8,18 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ezfb'
 ]).
-config(['$routeProvider', function($routeProvider) {
+config(['$routeProvider','ezfbProvider', function($routeProvider,ezfbProvider) {
   $routeProvider.when('/cronicas', {templateUrl: 'partials/cronicas.html', controller: 'MyCtrl1'});
   $routeProvider.when('/amolestados', {templateUrl: 'partials/amolestados.html', controller: 'MyCtrl1'});
   $routeProvider.when('/resultados', {templateUrl: 'partials/resultados.html', controller: 'MyCtrl1'});
   $routeProvider.when('/posiciones', {templateUrl: 'partials/posiciones.html', controller: 'MyCtrl1'});
   $routeProvider.when('/cronicas/:cancha', {templateUrl: 'partials/cronicas.html', controller: 'CronicasCtrl'});
   $routeProvider.otherwise({redirectTo: '/posiciones'});
+  
+  ezfbProvider.setInitParams({
+    appId: '386469651480295'
+  });
 }]);
