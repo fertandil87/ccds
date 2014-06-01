4b55dd626314626e099c1384cf49281e48601125
Fernando Saez <fertandil87@gmail.com>
2014-06-01 01:39:16 -0300

    agregada publicidad

--- js/controllers.js
+++ js/controllers.js
@@ -13,7 +13,7 @@ angular.module('myApp.controllers', [])
             $scope.cancha = $routeParams.cancha;
             $scope.trustSrc = function(src) {
                 return $sce.trustAsResourceUrl(src);
-              }
+            }
             $scope.cronicas = {
                 nahuel : "https://docs.google.com/document/d/1djp1wVgEvSr542M5HzqZmBu3_0vu36NoFgIvRD5E-70/pub?embedded=true",
                 talleres : "https://docs.google.com/document/d/1joQp3VuY9JU8ZZpSFoDJBGk64TG6Pn6mSIdGTnVp6S4/pub?embedded=true",
