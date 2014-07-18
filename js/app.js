d3011bfb26a5b9853adc5a357b258e6d0a17cff9
Fernando Saez <fertandil87@gmail.com>
2014-07-18 01:54:11 -0300

    added horarios pages

--- js/app.js
+++ js/app.js
@@ -12,12 +12,13 @@ angular.module('myApp', [
   'ezfb'
 ]).
 config(['$routeProvider','ezfbProvider', function($routeProvider,ezfbProvider) {
+  $routeProvider.when('/horarios', {templateUrl: 'partials/horarios.html', controller: 'MyCtrl1'});
   $routeProvider.when('/cronicas', {templateUrl: 'partials/cronicas.html', controller: 'MyCtrl1'});
   $routeProvider.when('/amolestados', {templateUrl: 'partials/amolestados.html', controller: 'MyCtrl1'});
   $routeProvider.when('/resultados', {templateUrl: 'partials/resultados.html', controller: 'MyCtrl1'});
   $routeProvider.when('/posiciones', {templateUrl: 'partials/posiciones.html', controller: 'MyCtrl1'});
   $routeProvider.when('/cronicas/:cancha', {templateUrl: 'partials/cronicas.html', controller: 'CronicasCtrl'});
-  $routeProvider.otherwise({redirectTo: '/posiciones'});
+  $routeProvider.otherwise({redirectTo: '/horarios'});
   
   ezfbProvider.setInitParams({
     appId: '386469651480295'
